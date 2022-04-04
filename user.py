adb40c6125ba8b69208cdd90b9eec601d728582f
altisseront <altisseront@gmail.com>
2022-04-04 14:35:39 -0400

    chaining methods update

--- user.py
+++ user.py
@@ -5,9 +5,10 @@ class User:
 
     def make_withdrawal(self, amount):
         self.balance -= amount
-
+        return self
     def make_deposit(self, amount):
         self.balance += amount
+        return self
 
     def display_user_balance(self):
         print(self.name, "=", self.balance)
@@ -22,22 +23,10 @@ jimmy = User("jimmy", 0)
 frederick = User("frederick", 0)
 
 
-gilbert.make_deposit(400)
-gilbert.make_deposit(200)
-gilbert.make_deposit(100)
-gilbert.make_withdrawal(50)
-gilbert.display_user_balance()
+gilbert.make_deposit(400).make_deposit(200).make_deposit(100).make_withdrawal(50).display_user_balance()
+jimmy.make_deposit(230).make_deposit(140).make_withdrawal(200).make_withdrawal(200).display_user_balance()
 
-jimmy.make_deposit(230)
-jimmy.make_deposit(140)
-jimmy.make_withdrawal(50)
-jimmy.make_withdrawal(500)
-jimmy.display_user_balance()
+frederick.make_deposit(150).make_withdrawal(30).make_withdrawal(40).make_withdrawal(20).display_user_balance()
 
-frederick.make_deposit(150)
-frederick.make_withdrawal(10)
-frederick.make_withdrawal(70)
-frederick.make_withdrawal(20)
-frederick.display_user_balance()
 
 jimmy.transfer_money(gilbert, 200)
